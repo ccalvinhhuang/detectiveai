942c5887679e33fa3176c6891ac114ac75a0ffbb
Andy Xu <xuandy05@gmail.com>
2025-03-24 18:09:02 -0700

    automated posts

--- src/constants.ts
+++ src/constants.ts
@@ -2,6 +2,11 @@ export const DEVVIT_SETTINGS_KEYS = {
   // Just here as an example!
   SECRET_API_KEY: 'SECRET_API_KEY',
   R2_BUCKET_URL: 'R2_BUCKET_URL',
+  ENABLE_AUTO_POSTING: 'ENABLE_AUTO_POSTING',
 } as const;
 
 export const WEBVIEW_ID = "webview";
+
+// Scheduler constants
+export const AUTO_POST_JOB_NAME = 'auto_post_every_six_hours';
+export const AUTO_POST_JOB_ID_KEY = 'auto_post_job_id';
